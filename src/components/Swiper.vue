7c1acb991c1aa0a8b5bf880aa96e018e2019c0fc
刘大维 <l729641074@163.com>
2023-07-15 16:54:34 +0800

    feat: 首页美化与数据对接

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,30 +1,30 @@
-
 <template>
   <van-swipe class="my-swipe" :autoplay="3000" indicator-color="#1baeae">
     <van-swipe-item v-for="(item, index) in list" :key="index">
-      <img :src="item.carouselUrl" alt="" @click="goTo(item.redirectUrl)">
+      <img :src="item.imgUrl" alt="" @click="goTo(item.id)" />
     </van-swipe-item>
   </van-swipe>
 </template>
 
-<script>
-export default {
-  props: {
-    list: Array
-  },
-  methods: {
-    goTo(url) {
-      window.open(url)
-    }
-  }
-}
+<script setup>
+import { defineProps } from "vue";
+import { useRouter } from "vue-router";
+defineProps({
+  list: Array,
+});
+const router = useRouter();
+const goTo = (id) => {
+  // window.open(url);
+  router.push({ path: `/product/${id}` });
+};
 </script>
 
-<style lang='less' scoped>
-  .my-swipe {
-    img {
-      width: 100%;
-      height: 100%;
-    }
+<style lang="less" scoped>
+.my-swipe {
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
   }
+}
 </style>
